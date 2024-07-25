1eb5dbfd3248bb13f6aecf57369824a1236a0b38
Terence Loughran <terryloughran@gmail.com>
2024-07-25 11:07:22 +0000

    Chore: move day_night to scenes doc

--- scenes.py
+++ scenes.py
@@ -1,4 +1,16 @@
-movements = {'beach':{'day':{'1': 'test text 1', '2': 'test text 2'}, 'night':'DEAD END'},
-             'cave': {'day':{}, 'night':{}},
-             'jungle': {'day':{}, 'night':{}},
-             'cliffs': {'day':{}, 'night':{}}}
+DAY_NIGHT = {
+    "day": [
+        "It is day time. You can see the sun shining brightly.\nThe weather is warm and the sky is clear. You can see the blue ocean and golden beach.\n\nStraight ahead is a dense jungle.\nTo your left you see a dark cave entrance.\nTo your right, you see a tall rocky cliff face.\n"
+    ],
+    "night": [
+        "It is night time. The moon is shining brightly.\nThe weather is cool and the sky is clear.\nYou can see the stars twinkling in the sky. You hear the sound of the ocean waves crashing on the shore.\n\nStraight ahead is a dark dense jungle.\nTo your left, you can just about make out a dark cave.\nTo your right, you see a steep rocky cliff.\n"
+    ],
+}
+
+
+movements = {
+    "beach": {"day": {"1": "test text 1", "2": "test text 2"}, "night": "DEAD END"},
+    "cave": {"day": {}, "night": {}},
+    "jungle": {"day": {}, "night": {}},
+    "cliffs": {"day": {}, "night": {}},
+}
