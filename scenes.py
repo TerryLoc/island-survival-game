c7878dccf6146d7a3fb74ac84b2360a8c0dd58e5
Terence Loughran <terryloughran@gmail.com>
2024-07-25 10:20:35 +0000

    Feat: add scenes file to create a dictionary

---
+++ scenes.py
@@ -0,0 +1,4 @@
+movements = {'beach':{'day':{'1': 'test text 1', '2': 'test text 2'}, 'night':'DEAD END'},
+             'cave': {'day':{}, 'night':{}},
+             'jungle': {'day':{}, 'night':{}},
+             'cliffs': {'day':{}, 'night':{}}}
